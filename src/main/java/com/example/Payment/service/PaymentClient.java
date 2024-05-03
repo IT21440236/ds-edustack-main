2b72ffe6c4211e3d739dc88214e5312c2b5fbaf4
it21312908 <it21312908@my.sliit.lk>
2024-05-03 08:24:17 +0530

    PaymentClient.java created

--- src/main/java/com/example/Payment/service/PaymentClient.java
+++ src/main/java/com/example/Payment/service/PaymentClient.java
@@ -1,13 +1,10 @@
 package com.example.Payment.service;
 
 import com.example.Payment.entity.CourseInfo;
-import com.example.Payment.entity.Learner;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
 
