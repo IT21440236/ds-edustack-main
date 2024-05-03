6a8aafcadf54aeba467b5e63932a8af8e6197369
it21312908 <it21312908@my.sliit.lk>
2024-05-03 08:25:19 +0530

    PaymentController.java modified

--- src/main/java/com/example/Payment/controller/PaymentController.java
+++ src/main/java/com/example/Payment/controller/PaymentController.java
@@ -1,21 +1,17 @@
 package com.example.Payment.controller;
 
-import com.example.Payment.entity.Completed_Payments;
 import com.example.Payment.entity.CourseInfo;
-import com.example.Payment.entity.EnrollRequest;
 import com.example.Payment.service.CompletedPaymentsImpl;
 import com.example.Payment.service.PaymentClient;
 import com.example.Payment.service.PaymentImpl;
-import com.example.Payment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("api/v1/payments")
 public class PaymentController {
 
@@ -40,24 +36,8 @@ public class PaymentController {
         }
     }
 
-//    @GetMapping("/{learnerId}/total-price")
-//    public ResponseEntity<Double> getTotalPriceForLearner(@PathVariable Long learnerId) {
-//        // Retrieve the learner's courses from the payment client
-//        ResponseEntity<Map<Long, CourseInfo>> response = paymentClient.getLearnerById(learnerId);
-//
-//        if (response != null && response.getBody() != null) {
-//            // Calculate the total price from the courses
-//            Map<Long, CourseInfo> courses = response.getBody();
-//            double totalPrice = paymentImpl.calculateTotalPrice(courses);
-//
-//            return ResponseEntity.ok(totalPrice);
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-
     @GetMapping("/{learnerId}/total-price")
-    public ResponseEntity<String> getTotalPriceAndCompletePayment(@PathVariable Long learnerId) {
+    public ResponseEntity<Double> getTotalPriceForLearner(@PathVariable Long learnerId) {
         // Retrieve the learner's courses from the payment client
         ResponseEntity<Map<Long, CourseInfo>> response = paymentClient.getLearnerById(learnerId);
 
@@ -66,32 +46,48 @@ public class PaymentController {
             Map<Long, CourseInfo> courses = response.getBody();
             double totalPrice = paymentImpl.calculateTotalPrice(courses);
 
-            // Complete payment by calling the completePayment endpoint internally
-            ResponseEntity<String> paymentResponse = completePayment(learnerId, totalPrice);
-            return ResponseEntity.ok("Total price: " + totalPrice + "\nPayment status: " + paymentResponse.getBody());
+            return ResponseEntity.ok(totalPrice);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-    @PostMapping("/completePayment")
-    private ResponseEntity<String> completePayment(Long learnerId, Double price) {
-        try {
-            Completed_Payments completedPayment = new Completed_Payments();
-            completedPayment.setLearnerId(String.valueOf(learnerId));
-            completedPayment.setPrice(price);
-            completedPayment.setDate(new Date());
+//    @GetMapping("/{learnerId}/total-price")
+//    public ResponseEntity<String> getTotalPriceAndCompletePayment(@PathVariable Long learnerId) {
+//        // Retrieve the learner's courses from the payment client
+//        ResponseEntity<Map<Long, CourseInfo>> response = paymentClient.getLearnerById(learnerId);
+//
+//        if (response != null && response.getBody() != null) {
+//            // Calculate the total price from the courses
+//            Map<Long, CourseInfo> courses = response.getBody();
+//            double totalPrice = paymentImpl.calculateTotalPrice(courses);
+//
+//            // Complete payment by calling the completePayment endpoint internally
+//            ResponseEntity<String> paymentResponse = completePayment(learnerId, totalPrice);
+//            return ResponseEntity.ok("Total price: " + totalPrice + "\nPayment status: " + paymentResponse.getBody());
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+//    @PostMapping("/completePayment")
+//    private ResponseEntity<String> completePayment(Long learnerId, Double price) {
+//        try {
+//            Completed_Payments completedPayment = new Completed_Payments();
+//            completedPayment.setLearnerId(String.valueOf(learnerId));
+//            completedPayment.setPrice(price);
+//            completedPayment.setDate(new Date());
 //            completedPayment.setCurrency(completedPayment.getCurrency());
 //            completedPayment.setDescription(completedPayment.getDescription());
 //            completedPayment.setIntent(completedPayment.getIntent());
 //            completedPayment.setMethod(completedPayment.getMethod());
-
-            completedPaymentsImpl.saveCompletedPayment(completedPayment);
-            return ResponseEntity.ok("Payment completed and saved successfully.");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error occurred while completing payment: " + e.getMessage());
-        }
-    }
+//
+//            completedPaymentsImpl.saveCompletedPayment(completedPayment);
+//            return ResponseEntity.ok("Payment completed and saved successfully.");
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("Error occurred while completing payment: " + e.getMessage());
+//        }
+//    }
 
 }
